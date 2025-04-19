5101ca9d0b7d4225c688c90ab4c64bf2ca0e2285
Adeyemi Damilare Adeoye <58517568+adeyemiadeoye@users.noreply.github.com>
2025-04-19 18:38:41 +0200

    removed unnecessary includes

--- src/SCSONumericalTests.jl
+++ src/SCSONumericalTests.jl
@@ -1,13 +1,4 @@
 module SCSONumericalTests
 
-include("../examples/spgrouplasso/get_group.jl")
-include("../examples/spgrouplasso/spgrouplasso.jl")
-include("../examples/load_bcd.jl")
-include("../examples/load_ssnal.jl")
-include("../examples/spdeconv.jl")
-include("../examples/splogl1.jl")
-include("../examples/exp-utils-paper.jl")
-include("../examples/run-experiments.jl")
-
 
 end
